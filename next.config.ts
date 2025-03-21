0e4bd8fd5c531482259e4df0de54b27afa8f5692
iseyoshitaka <ise0615@gmail.com>
2025-03-21 20:38:27 +0900

    Github pageで動くように修正 https://isystk.github.io/react19-hook-study/

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,19 @@
 import type {NextConfig} from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
     /* config options here */
     env: {
-        APP_NAME: 'React Study',
+        APP_NAME: 'React19 Hook Study',
         APP_DESCRIPTION: 'Reactの学習用サンプルアプリケーションです。NextJS15を利用してReact19の主要なHooksの利用方法を学習します。',
     },
+    // GitHub Pages の場合、コンテキストパス（basePath）を設定
+    basePath: isProd ? "/react19-hook-study" : "",
+    assetPrefix: isProd ? "/react19-hook-study/" : "",
+    // Static Export を有効化
     output: "export",
+    trailingSlash: true, // ファイルパスの末尾に `/` を付ける
 };
 
 export default nextConfig;
