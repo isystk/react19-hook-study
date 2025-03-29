6f8165fc0b9910d621684ea400a38b5a802d92e9
iseyoshitaka <ise0615@gmail.com>
2025-03-29 13:21:51 +0900

    Logo.tsxはprocessのEnvを参照

--- src/components/molecules/Logo/index.tsx
+++ src/components/molecules/Logo/index.tsx
@@ -1,8 +1,5 @@
 import styles from './styles.module.scss';
 import Image from '@/components/atoms/Image';
-import getConfig from 'next/config';
-const { publicRuntimeConfig } = getConfig();
-
 
 export type Props = object
 
@@ -10,7 +7,7 @@ const Logo = () => {
     return (
         <div className={styles.logo}>
             <Image src="/images/github-mark-white.svg" width={30} height={30} alt="Logo" />
-            <p>{publicRuntimeConfig.APP_NAME}</p>
+            <p>{process.env.APP_NAME}</p>
         </div>
     );
 };
